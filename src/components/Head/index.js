b41f5491e274ada2f131bf9e75e3482e612ae50e
noebezerra <noebezerra@hotmail.com>
2021-01-26 01:34:33 -0300

    meta type png

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -16,7 +16,7 @@ const HtmlHead = ({ db }) => {
       <meta property="og:site_name" content={db.title} />
       <meta property="og:description" content={db.description} />
       <meta property="og:image" content={db.bg} />
-      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:type" content="image/jpg" />
       <meta property="og:image:width" content="640" />
       <meta property="og:image:height" content="442" />
       <meta property="og:type" content="website" />
